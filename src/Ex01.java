76c861907da0a5287b16c61e2de759ee855430b2
minami <minami.yajima@rakus-partners.co.jp>
2025-01-20 10:25:13 +0900

    first commit

---
+++ src/Ex01.java
@@ -0,0 +1,10 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        String message = "I have a dream today";
+
+        int stringLength=message.length();
+
+        System.out.println(stringLength+"文字目です");
+    }
+
+}
